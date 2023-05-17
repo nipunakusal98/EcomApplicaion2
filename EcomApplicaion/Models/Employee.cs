59bdb12616a844f1cb5426f981066a5bcb078f9b
WORK <WORK@DESKTOP-K1M21UD>
2023-05-18 02:52:24 +0530

    Initial

---
+++ EcomApplicaion/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcomApplicaion.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmpID { get; set; }
+        public string EmpName { get; set; }
+        public string EmpEmail { get; set; }
+        public string EmpPassword { get; set; } 
+        public string EmpType { get; set; }
+    }
+}
